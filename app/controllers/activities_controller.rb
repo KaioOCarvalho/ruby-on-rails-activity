ed15886429c6917facf27a2246c2c75fbbf9947b
Vinicius Teixeira Hirschle <vinicius.teixeira@cubos.io>
2021-11-08 15:02:32 +0000

    implementação do exercício

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,14 +4,22 @@ class ActivitiesController < ApplicationController
 
     def index
         # listar as atividades (variável activities)
-
+        @activities = Activity.all
         # criar uma atividade (variável activity)
+        @activity = Activity.new(name: "Atividade", date: Time.now.strftime("%d/%m/%Y"), description: "Descrição da atividade")
     end
     
     def create
         #Crie uma atividade aqui
-
+        @activity = Activity.new(activity_params)
         #Envie uma mensagem com flash[:msg] contendo o alerta
+        begin
+            @activity.save
+            @alert = "Atividade criada com sucesso!"
+        rescue
+            @alert = "Erro ao criar atividade!"
+        end
+        flash[:msg] = @alert
 
         write_alert_on_log
         redirect_to activities_index_url
@@ -19,9 +27,15 @@ class ActivitiesController < ApplicationController
 
     def destroy
         #Destrua uma atividade por id
-
+        @activity = Activity.find(params[:id])
+        begin
+            @activity.destroy
+            @alert = "Atividade removida com sucesso!"
+        rescue
+            @alert = "Erro ao remover atividade!"
+        end
         #Envie uma mensagem com flash[:msg] contendo o alerta
-
+        flash[:msg] = @alert
         write_alert_on_log
         redirect_to activities_index_url
     end
@@ -29,7 +43,7 @@ class ActivitiesController < ApplicationController
     private
 
     def activity_params
-        #Defina os parâmetros de cada atividade em params
+        params.require(:activity).permit(:name, :date, :description)
     end
 
     def write_alert_on_log
